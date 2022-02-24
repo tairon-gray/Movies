512d133646f351db837e8d1a545a5f65f2d18f9a
Eugene Ivanov <wied11@yandex.ru>
2022-02-24 14:31:35 +0500

    Загрузка данных из сети

--- app/src/main/java/com/evg_ivanoff/movies/MainActivity.java
+++ app/src/main/java/com/evg_ivanoff/movies/MainActivity.java
@@ -3,6 +3,16 @@ package com.evg_ivanoff.movies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.evg_ivanoff.movies.data.Movie;
+import com.evg_ivanoff.movies.utils.JsonUtils;
+import com.evg_ivanoff.movies.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        ArrayList<Movie> movies = JsonUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie : movies){
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.i("MyResult", builder.toString());
     }
 }
